38f9d326b188d2b39717caacf098ce83a203b247
Alejandro Lopez <lopez.alejandro@dali.dartmouth.edu>
2024-11-05 12:31:06 -0500

    Initial commit

---
+++ Commands/Basic.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace DiscordBotTemplateNet8.Commands
+{
+    public class Basic : BaseCommandModule
+    {
+        [Command("hello")]
+        [Description("Says hello")]
+        [Aliases("hi")]
+        public async Task HelloCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello!");
+        }
+
+        [Command("bye")]
+        [Description("Says bye")]
+        public async Task ByeCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Bye Bye!");
+        }
+    }
+}
